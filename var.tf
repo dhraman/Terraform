4125d83c37023af34269975451f3b0d86a82e3f0
dhraman <dharika.raman@capgemini.com>
2017-02-01 18:36:29 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,4 +7,4 @@ variable "instance_ type" { default = "gl-small"
 }
  variable "image_id" { default = "ubuntu-1604-lts" #default = "rhel-7"
 } 
-variable "instance name" { — default = "gce-instance" }
+variable "instance name" { default = "gce-instance" }
