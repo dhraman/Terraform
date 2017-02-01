9cf4f30e231ac4d75ad76c405a3edccdb8958307
dhraman <dharika.raman@capgemini.com>
2017-02-01 18:29:21 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "region" { default = "us-centrall"
+} 
+variable "region_zone" { default = "us-centrall-c"
+}
+ variable "project_name" { default = "ipaas-150211" description = "The ID of the Google Cloud project" } 
+variable "instance_ type" { default = "gl-small"
+}
+ variable "image_id" { default = "ubuntu-1604-lts" #default = "rhel-7"
+} 
+variable "instance name" { — default = "gce-instance" }
